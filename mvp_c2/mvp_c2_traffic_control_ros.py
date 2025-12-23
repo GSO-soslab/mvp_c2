70a99eea92f7abccae8760914938f43651f01699
mzhouURI <mzhou@uri.edu>
2025-12-23 11:54:16 -0500

    update udp

--- mvp_c2/mvp_c2_traffic_control_ros.py
+++ mvp_c2/mvp_c2_traffic_control_ros.py
@@ -87,7 +87,7 @@ class TrafficControlRos(Node):
         dccl_msg = bytearray([])
 
         for i in range(len(data)):
-            dccl_msg = dccl_msg.extend(data[i])
+            dccl_msg = dccl_msg.append(data[i])
 
             if len(dccl_msg) >= 4 and dccl_msg[-4] == 42: #the four last chars are *AB\n
                 msg = ByteMultiArray()
