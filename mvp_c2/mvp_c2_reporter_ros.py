1ac08420a7a906677ddce6e92251306c980ae7b9
mzhouURI <mzhou@uri.edu>
2025-12-27 12:58:49 -0500

    added rx interval check in reporter/commander

--- mvp_c2/mvp_c2_reporter_ros.py
+++ mvp_c2/mvp_c2_reporter_ros.py
@@ -38,7 +38,9 @@ class MvpC2Reporter(Node):
 
         self.local_id = self.declare_parameter('local_id', 2).value
         self.remote_id = self.declare_parameter('remote_id', 1).value
-        self.dccl_rx_interval = self.declare_parameter('dccl_rx_interval', 2.0).value
+        self.dccl_rx_interval = self.declare_parameter('dccl_rx_interval', 1.0).value
+        self.dccl_tx_interval = self.declare_parameter('dccl_tx_interval', 1.0).value
+
         self.local_mvp_active = self.declare_parameter('local_mvp_active', True).value
         self.ser_wait_time = self.declare_parameter('service_wait_time', 0.2).value
 
