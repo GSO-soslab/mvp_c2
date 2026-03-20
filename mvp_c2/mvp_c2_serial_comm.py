6caba91d75dd6167df997d5d3b51f5acaacc0d3c
mzhouURI <mzhou@uri.edu>
2026-03-19 21:45:33 -0400

    added \n check in serial rx

--- mvp_c2/mvp_c2_serial_comm.py
+++ mvp_c2/mvp_c2_serial_comm.py
@@ -51,7 +51,7 @@ class MvpC2SerialRos(Node):
                     data = data + temp_data
                     counter = counter + 1
 
-                    if len(data) >= 4 and data[-4] == 42: #the four last chars are *AB\n
+                    if len(data) >= 4 and data[-4] == 42 and data[-1]==ord('\n'): #the four last chars are *AB\n
                         msg = ByteMultiArray()
                         msg.data = data
                         # print(msg.data)
